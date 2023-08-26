a0f5c1cc0d7882b969e67f30e435a00704adeb49
Satiz004 <138674478+Satiz004@users.noreply.github.com>
2023-08-27 00:00:34 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
         https = "https"
         link = link.replace("http", https)
 
-    url = f'https://indianshortner.com/shortLink'
+    url = f'https://indianshortner.in/shortlink'
     params = {'token': URL_SHORTNER_WEBSITE_API,
               'link': link,
               'format': 'json'
@@ -397,8 +397,8 @@ async def get_shortlink(link):
                     return data['shortlink']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https://indianshortner.com/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+                    return f'https://indianshortner.in/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
 
     except Exception as e:
         logger.error(e)
-        return f'https://indianshortner.com/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+        return f'https://indianshortner.in/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
